fe8f0339a2fd7761e26d221d2df28d714888a7c7
KjellConnelly <kjellapps@gmail.com>
2018-11-06 10:15:17 -0800

    Next Version

--- index.js
+++ index.js
@@ -2,10 +2,44 @@ import { Platform, Linking, NativeModules } from 'react-native'
 import CloudKitJS from './local_versions/1.0/cloudkit'
 
 export default class CloudKit {
-  static init(options, callback) {
+  static init(options) {
     const configured = CloudKitJS.configure({
       containers: options.containers
     })
-    callback(CloudKitJS)
+    return CloudKitJS
+  }
+
+  static async query(options = {
+
+  }) {
+    return new Promise((resolve, reject)=>{
+
+    })
+  }
+
+  static fetchRecordsByName(recordName, options = {
+    container:"default",
+    database:"public"
+  }) {
+    return new Promise((resolve, reject)=>{
+      const container = options.container == "default" ?
+        CloudKitJS.getDefaultContainer() :
+        CloudKitJS.getContainer(options.container)
+
+      const database = options.database == "private" ?
+        container.privateCloudDatabase : (
+          options.database == "shared" ?
+            container.sharedCloudDatabase :
+            container.publicCloudDatabase
+        )
+
+      database.fetchRecords(recordName).then(response=>{
+        if (response.hasErrors) {
+          reject(response.errors)
+        } else {
+          resolve(response.records)
+        }
+      })
+    })
   }
 }
